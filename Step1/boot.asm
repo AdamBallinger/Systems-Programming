22b8bcd81eb6847eb0bba032699df1bcd857e953
Adam Ballinger <crunchienut20@gmail.com>
2016-09-22 21:20:00 +0100

    Added CRLF and WriteLine functions.

--- Step1/boot.asm
+++ Step1/boot.asm
@@ -23,13 +23,25 @@ Console_Write_16:
 
 Console_Write_16_Repeat:
 	lodsb								; Load byte at SI into AL and increment SI
-    test 	al, al						; If the byte is 0, we are done
+	test 	al, al						; If the byte is 0, we are done
 	je 		Console_Write_16_Done
 	int 	10h							; Output character to screen
 	jmp 	Console_Write_16_Repeat
 
 Console_Write_16_Done:
     ret
+	
+Console_Write_CRLF: ; Moves the cursor to a new line
+	mov		al, 0Dh ; Moves the carriage return character into the lower order AX registry
+	int		10h	; Output the character to screen.
+	mov		al, 0Ah ; Moves the line feed character to the lower order AX registry
+	int		10h ; Output the character to screen.
+	ret
+	
+Console_WriteLine_16 ; Writes a message to a the screen followed by a new line
+	call Console_Write_16 ; Write the message.
+	call Console_Write_CRLF ; Move to new line.
+	ret
 
 ;	Start of the actual boot loader code
 	
@@ -42,12 +54,12 @@ Real_Mode_Start:
     mov 	ds, ax						; Set data segment (DS) to 0.
 	
 	mov 	si, boot_message			; Display our greeting
-	call 	Console_Write_16
+	call 	Console_WriteLine_16
 	
 	hlt									; Halt the processor
 	
 ; Data
-boot_message:	db	'UODos V1.0', 0
+boot_message:	db	'KappaOS v4.20', 0 ; Message followed by null character (0)
 
 ; Pad out the boot loader so that it will be exactly 512 bytes
 	times 510 - ($ - $$) db 0
