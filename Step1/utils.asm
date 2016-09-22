67d9ed88e516b9dd9146ec76b96c89168a0b6b9c
Adam Ballinger <crunchienut20@gmail.com>
2016-09-22 21:26:37 +0100

    Moved Console functions to utils.asm

---
+++ Step1/utils.asm
@@ -0,0 +1,30 @@
+
+
+; Write to the console using BIOS.
+; 
+; Input: SI points to a null-terminated string
+
+Console_Write_16:
+	mov 	ah, 0Eh						; BIOS call to output value in AL to screen
+
+Console_Write_16_Repeat:
+	lodsb								; Load byte at SI into AL and increment SI
+	test 	al, al						; If the byte is 0, we are done
+	je 		Console_Write_16_Done
+	int 	10h							; Output character to screen
+	jmp 	Console_Write_16_Repeat
+
+Console_Write_16_Done:
+    ret
+	
+Console_Write_CRLF: ; Moves the cursor to a new line
+	mov		al, 0Dh ; Moves the carriage return character into the lower order AX registry
+	int		10h	; Output the character to screen.
+	mov		al, 0Ah ; Moves the line feed character to the lower order AX registry
+	int		10h ; Output the character to screen.
+	ret
+	
+Console_WriteLine_16 ; Writes a message to a the screen followed by a new line
+	call Console_Write_16 ; Write the message.
+	call Console_Write_CRLF ; Move to new line.
+	ret
