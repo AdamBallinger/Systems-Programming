dff02f674ff657ff37387a34fca5a04072ed5033
Jamie Sharpe <github@jamiesharpe.co.uk>
2016-09-26 15:38:32 +0100

    Different style

--- Step1/utils.asm
+++ Step1/utils.asm
@@ -3,29 +3,29 @@
 ; Input: SI points to a null-terminated string
 
 Console_Write_16:
-	mov 	ah, 0Eh						; BIOS call to output value in AL to screen
-	call Console_Write_16_Repeat
-	ret 	; end of write
+    MOV     ah, 0Eh                     ; BIOS call to output value in AL to screen
+    CALL Console_Write_16_Repeat
+    RET     ; end of write
 
 Console_Write_16_Repeat: 
-	lodsb								; Load byte at SI into AL and increment SI
-	test 	al, al						; If the byte is 0, we are done
-	je 		Console_Write_16_Done
-	int 	10h							; Output character to screen
-	jmp 	Console_Write_16_Repeat
-	ret		; end of write repeat
+    LODSB                               ; Load byte at SI into AL and increment SI
+    TEST    al, al                      ; If the byte is 0, we are done
+    JE      Console_Write_16_Done
+    INT     10h                         ; Output character to screen
+    JMP     Console_Write_16_Repeat
+    RET     ; end of write repeat
 
 Console_Write_16_Done:
-    ret
-	
+    RET
+    
 Console_Write_CRLF: ; Moves the cursor to a new line
-	mov		al, 0Dh ; Moves the carriage return character into the lower order AX registry
-	int		10h	; Output the character to screen.
-	mov		al, 0Ah ; Moves the line feed character to the lower order AX registry
-	int		10h ; Output the character to screen.
-	ret		; end of write crlf
-	
+    MOV     al, 0Dh ; Moves the carriage return character into the lower order AX registry
+    INT     10h ; Output the character to screen.
+    MOV     al, 0Ah ; Moves the line feed character to the lower order AX registry
+    INT     10h ; Output the character to screen.
+    RET     ; end of write crlf
+    
 Console_WriteLine_16: ; Writes a message to a the screen followed by a new line
-	call Console_Write_16 ; Write the message.
-	call Console_Write_CRLF ; Move to new line.
-	ret		; end of write line
+    CALL Console_Write_16 ; Write the message.
+    CALL Console_Write_CRLF ; Move to new line.
+    RET     ; end of write line
