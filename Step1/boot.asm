090ec3cc4655a98e5c4559d53b0ea938a36899e5
Jamie Sharpe <github@jamiesharpe.co.uk>
2016-09-26 15:32:28 +0100

    Clean up and function call styling.

--- Step1/boot.asm
+++ Step1/boot.asm
@@ -9,61 +9,41 @@ BITS 16
 ; Tell the assembler that we will be loaded at 7C00 (That's where the BIOS loads boot loader code).
 
 ORG 7C00h
-    jmp     Real_Mode_Start             ; Jump past our sub-routines
+    JMP     Real_Mode_Start             ; Jump past our sub-routines
 
 %include "utils.asm"
 
 ;   Start of the actual boot loader code
-    
-; Loop_Test:        ; Output the characters 'L' and 'O' over and over until cx is 0
-    ; mov   al, 4Ch
-    ; int   10h
-    ; mov   al, 4Fh
-    ; int   10h
-    ; mov       al, 178
-    ; int       10h
-    ; loop  Loop_Test   ; if cx is not 0 then call Loop_Test. cx is decremented by 1 each time the loop iterates.
-    ; ret       ; end of loop   
-    
-; Loop_Line_16:
-    ; mov   si, boot_message            ; Display our greeting
-    ; call  Console_WriteLine_16
-    ; loop  Loop_Line_16
-    ; ret
-
 Real_Mode_Start:
-    cli                                 ; Prevent hardware interrupts occuring during the boot process}
-    xor     ax, ax                      ; Set stack segment (SS) to 0 and set stack size to 4K
-    mov     ss, ax
-    mov     sp, 4000h
+    CLI                                 ; Prevent hardware interrupts occuring during the boot process}
+    XOR     ax, ax                      ; Set stack segment (SS) to 0 and set stack size to 4K
+    MOV     ss, ax
+    MOV     sp, 4000h
 
-    mov     ds, ax                      ; Set data segment (DS) to 0.
+    MOV     ds, ax                      ; Set data segment (DS) to 0.
     
-    mov     si, boot_message            ; Display our greeting
-    call    Console_WriteLine_16
-    
-    ;mov    cx, 30      ; cx stores the number of times the loop function should run and is auto decremented when loop is called.
-    ;call   Loop_Test
+    MOV     si, boot_message            ; Display our greeting
+    CALL    Console_WriteLine_16
 
-    hlt                                 ; Halt the processor
-    ret                                 ; End of real mode
+    HLT                                 ; Halt the processor
+    RET                                 ; End of real mode
     
 ; Data
 
 ; Boot Message followed by null character (0)
 boot_message:
-        db  " #####                              #######  ##### ", 0Dh, 0Ah
-        db  "#     # #    #   ##    ####  #    # #     # #     #", 0Dh, 0Ah
-        db  "#     # #    #  #  #  #    # #   #  #     # #      ", 0Dh, 0Ah
-        db  "#     # #    # #    # #      ####   #     #  ##### ", 0Dh, 0Ah
-        db  "#   # # #    # ###### #      #  #   #     #       #", 0Dh, 0Ah
-        db  "#    #  #    # #    # #    # #   #  #     # #     #", 0Dh, 0Ah
-        db  " #### #  ####  #    #  ####  #    # #######  ##### ", 0
+        DB  " #####                              #######  ##### ", 0Dh, 0Ah
+        DB  "#     # #    #   ##    ####  #    # #     # #     #", 0Dh, 0Ah
+        DB  "#     # #    #  #  #  #    # #   #  #     # #      ", 0Dh, 0Ah
+        DB  "#     # #    # #    # #      ####   #     #  ##### ", 0Dh, 0Ah
+        DB  "#   # # #    # ###### #      #  #   #     #       #", 0Dh, 0Ah
+        DB  "#    #  #    # #    # #    # #   #  #     # #     #", 0Dh, 0Ah
+        DB  " #### #  ####  #    #  ####  #    # #######  ##### ", 0
 
 
 ; Pad out the boot loader so that it will be exactly 512 bytes
-    times 510 - ($ - $$) db 0
+    TIMES 510 - ($ - $$) db 0
     
 ; The segment must end with AA55h to indicate that it is a boot sector
-    dw 0AA55h
+    DW 0AA55h
     
