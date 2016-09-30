a9b9b972b6695c2c18d92bf1b16b6f4ec1b928e6
Adam Ballinger <crunchienut20@gmail.com>
2016-09-30 20:09:56 +0100

    dunno

--- Step1/boot2.asm
+++ Step1/boot2.asm
@@ -1,5 +1,3 @@
-;BITS 16
-
 ORG 9000h
 
 %include "utils.asm"
@@ -8,16 +6,15 @@ ORG 9000h
 jmp Boot_Stage_2
 
 Boot_Stage_2:
-	
-	mov	si, stage_start_message
-	call	Console_WriteLine_16
+	;mov		si, stage_start_message
+	;call	Console_WriteLine_16
 	
 	hlt
 	
 stage_start_message: db "Stage 2 of boot started", 0
 
 ; Pad out the boot loader so that it will be exactly 2560 bytes
-	times 2558 - ($ - $$) db 0
+;	times 2558 - ($ - $$) db 0
 	
 ; The segment must end with AA55h to indicate that it is a boot sector
-	dw 0AA55h
+;	dw 0AA55h
